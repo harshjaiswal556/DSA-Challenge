50e90229d33f8becc991859e4c852831a2659340
Harsh Shah Jaiswal <92356100+harshjaiswal556@users.noreply.github.com>
2022-07-31 16:52:57 +0530

    ques1 done

---
+++ question1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class question1 {
+    static void reverse(int[] arr, int n) {
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr, n);
+    }
+}
